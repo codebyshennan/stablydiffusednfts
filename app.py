2b2c5053abf63d92a699645b4d98beac8da018d0
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-11-11 23:41:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,37 @@
-# import streamlit as st
-# from PIL import Image
-# from audio_recorder_streamlit import audio_recorder
-# import whisper
-# import pathlib
-# from pathlib import Path
-
-# st.title("Stable Diffusion")
-
-# st.write("Loading model...")
-
-# model = whisper.load_model("tiny")
-
-# st.write("Model loaded!")
-
-# HERE = Path(__file__).parent
-# print(HERE)
-
-# audio_bytes = audio_recorder()
-
-# if audio_bytes:
-# 	st.audio(audio_bytes, format="audio/wav")
-
-# 	with open('myfile.wav', mode='bw') as f:
-# 		f.write(audio_bytes)
-
-# 	path = f"{HERE}\\myfile.wav"
-
-# 	st.write(path)
-
-# 	out = model.transcribe(path)
-# 	st.write(out['text'])
-
+from audio_recorder_streamlit import audio_recorder
+import whisper
+import pathlib
+from pathlib import Path
 from tempfile import NamedTemporaryFile
-
 import streamlit as st
 import whisper
-
 import ffmpeg
-st.write("ffmpeg imported")
+
+st.title("Stable Diffusion")
+
+st.write("Loading model...")
+
+model = whisper.load_model("base")
+
+st.write("Model loaded!")
+
+HERE = Path(__file__).parent
+print(HERE)
+
+audio_bytes = audio_recorder()
+
+if audio_bytes:
+ 	st.audio(audio_bytes, format="audio/wav")
+
+ 	with open('myfile.wav', mode='bw') as f:
+ 		f.write(audio_bytes)
+
+	path = f"{HERE}\\myfile.wav"
+
+ 	st.write(path)
+
+ 	out = model.transcribe(path)
+ 	st.write(out['text'])
 
 audio = st.file_uploader("Upload an audio file", type=["wav"])
 
@@ -45,6 +39,5 @@ if audio is not None:
     with NamedTemporaryFile(suffix="wav") as temp:
         temp.write(audio.getvalue())
         temp.seek(0)
-        model = whisper.load_model("base")
         result = model.transcribe(temp.name)
         st.write(result["text"])
